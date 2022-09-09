6b28a1db6a52877ed1f38b90e6f857bd47676fe7
dwivediom00 <dwivediom00.com>
2022-09-09 20:27:40 +0530

    stack.2

--- 9Stack/stackOprations.cpp
+++ 9Stack/stackOprations.cpp
@@ -21,28 +21,60 @@ class Stack {
 Stack:: Stack(int size){ 
     this->size = size ; 
     top= -1; 
-    int *S = new int[size]; 
+    S = new int[size]; 
 
 }
 Stack :: ~Stack(){ 
     delete[] S ; 
 }
 void Stack :: push(int val ){ 
-     if(top==this->size-1){ 
+    
+     if(top==size-1){ 
         cout<< "Stack overflow"; 
      }else{ 
+      
         top++ ; 
-        S[top]= val; 
+       
+       S[top]= val; 
+          
      }
 
 }
 
 void Stack :: display(){ 
+     cout<< "disply wroking "; 
     for (int i =0 ; i<size ; i++){ 
         cout<< S[i]; 
     }
 }
+int Stack :: pop(){ 
+    int x =1 ; 
+    if(top==-1 ){ 
+        cout<< "stack is underflow "; 
 
-int main (){ 
-    
+    }else{ 
+            x = S[top]; 
+            top--; 
+    }
+    cout<< endl<< x << endl  ; 
+    return x ; 
 }
+int Stack :: peek(int index ){ 
+     if(index > size && index < 0 ){ 
+        cout<< "invalid index "; 
+     }
+    int sindex = size-index + 1 ; 
+    cout <<endl << S[sindex]<< endl ; 
+    return S[sindex]; 
+}
+int main (){ 
+     int size = 5 ; 
+    Stack stk( size) ; 
+    for(int i = 1 ; i<=size ; i++){ 
+        stk.push(i);
+    }
+    stk.display(); 
+    stk.pop();
+    stk.peek(3); 
+  return 0 ;  
+}         
